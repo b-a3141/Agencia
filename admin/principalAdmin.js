96dd0646d94832dcd477de6d37ac2c80dea3f978
BerardiAle <berardi.alejandro@gmail.com>
2021-11-26 09:48:28 -0300

    admin con alta usuario

--- admin/principalAdmin.js
+++ admin/principalAdmin.js
@@ -1,11 +1,3 @@
 /**
  * 
  */
-let btnAdminProductos = document.getElementById("btnAdminProductos");
-let btnAdminUsuarios = document.getElementById("btnAdminUsuarios");
-
-btnAdminProductos.addEventListener("click", function(){
-  return location.href = 'administrarProductos.html'});
-
-btnAdminUsuarios.addEventListener("click", function(){
-  return location.href = 'administrarUsuarios.html'});
